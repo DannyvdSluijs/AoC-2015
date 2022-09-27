1d0de8c1460339ea3bcadbc0b4a910db1fd8188f
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2022-09-27 09:03:58 +0200

    feat: Solution for Day 3

--- run.php
+++ run.php
@@ -3,6 +3,6 @@
 
 require_once 'vendor/autoload.php';
 
-$answer = (new \Dannyvdsluijs\AdventOfCode2015\Day02())->partTwo();
+$answer = (new \Dannyvdsluijs\AdventOfCode2015\Day03())->partTwo();
 
 printf("The correct answer is: %s\r\n", $answer);
---
+++ src/Day03.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dannyvdsluijs\AdventOfCode2015;
+
+class Day03
+{
+    public function partOne(): string
+    {
+        $content = file_get_contents(__DIR__ . '/../inputs/day3.txt');
+        $moves = str_split($content);
+
+        $pos = (object)['x' => 0, 'y' => 0];
+        $deliveredAt = ['0,0' => 1];
+
+        foreach ($moves as $move) {
+            $this->move($move, $pos);
+            $deliveredAt = $this->addDelivery($pos, $deliveredAt);
+        }
+
+        $result = array_filter($deliveredAt, static fn($i) => $i >= 1);
+
+        return (string) count($result);
+    }
+
+    public function partTwo(): string
+    {
+        $content = file_get_contents(__DIR__ . '/../inputs/day3.txt');
+        $moves = str_split($content);
+
+        $santaPos = (object)['x' => 0, 'y' => 0];
+        $robotPos = (object)['x' => 0, 'y' => 0];
+        $deliveredAt = ['0,0' => 2];
+
+        $size = count($moves);
+        for ($x = 0; $x < $size; $x++) {
+            $this->move($moves[$x], $santaPos);
+            $deliveredAt = $this->addDelivery($santaPos, $deliveredAt);
+
+            $x++;
+
+            $this->move($moves[$x], $robotPos);
+            $deliveredAt = $this->addDelivery($robotPos, $deliveredAt);
+        }
+
+        $result = array_filter($deliveredAt, static fn($i) => $i >= 1);
+
+        return (string) count($result);
+    }
+
+    private function move(string $move, \stdClass $position): void
+    {
+        match ($move) {
+            '^' => $position->y++,
+            '>' => $position->x++,
+            'v' => $position->y--,
+            '<' => $position->x--,
+        };
+    }
+
+    private function getPositionKey(\stdClass $position): string
+    {
+        return $position->x . ',' . $position->y;
+    }
+
+    private function addDelivery(\stdClass $position, array $currentDeliveries): array
+    {
+        $key = $this->getPositionKey($position);
+        if (!array_key_exists($key, $currentDeliveries)) {
+            $currentDeliveries[$key] = 0;
+        }
+        $currentDeliveries[$key]++;
+        printf("Delivered present at %s, total presents is %d\r\n", $key, $currentDeliveries[$key]);
+        return $currentDeliveries;
+    }
+}
